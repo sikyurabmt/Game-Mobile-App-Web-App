c9b19c4661e4cb9aeb3eeebcfe1a39aaecfc5afd
Nguyen Quang <sikyurabmt@gmail.com>
2015-12-07 15:59:50 +0700

    com

---
+++ Data Source/DIDONG/Source/MP Test/BackgroundTask/BackgroundAudioTask.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Playback;
+
+namespace BackgroundTask
+{
+    public sealed class BackgroundAudioTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+        private SystemMediaTransportControls _systemMediaTransportControl;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _systemMediaTransportControl = SystemMediaTransportControls.GetForCurrentView();
+            _systemMediaTransportControl.IsEnabled = true;
+
+            BackgroundMediaPlayer.MessageReceivedFromForeground += MessageReceivedFromForeground;
+            BackgroundMediaPlayer.Current.CurrentStateChanged += BackgroundMediaPlayerCurrentStateChanged;
+
+            // Associate a cancellation and completed handlers with the background task.
+            taskInstance.Canceled += OnCanceled;
+            taskInstance.Task.Completed += Taskcompleted;
+
+            _deferral = taskInstance.GetDeferral();
+        }
+
+        private void MessageReceivedFromForeground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            ValueSet valueSet = e.Data;
+            foreach (string key in valueSet.Keys)
+            {
+                switch (key)
+                {
+                    case "Play":
+                        Debug.WriteLine("Starting Playback");
+                        Play(valueSet[key].ToString());
+                        break;
+                }
+            }
+        }
+
+        private void Play(string toPlay)
+        {
+            MediaPlayer mediaPlayer = BackgroundMediaPlayer.Current;
+            mediaPlayer.AutoPlay = true;
+            mediaPlayer.SetUriSource(new Uri(toPlay));
+
+            //Update the universal volume control
+            _systemMediaTransportControl.ButtonPressed += MediaTransportControlButtonPressed;
+            _systemMediaTransportControl.IsPauseEnabled = true;
+            _systemMediaTransportControl.IsPlayEnabled = true;
+            _systemMediaTransportControl.DisplayUpdater.Type = MediaPlaybackType.Music;
+            _systemMediaTransportControl.DisplayUpdater.MusicProperties.Title = "Test Title";
+            _systemMediaTransportControl.DisplayUpdater.MusicProperties.Artist = "Test Artist";
+            _systemMediaTransportControl.DisplayUpdater.Update();
+        }
+
+        /// <summary>
+        ///     The MediaPlayer's state changes, update the Universal Volume Control to reflect the correct state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void BackgroundMediaPlayerCurrentStateChanged(MediaPlayer sender, object args)
+        {
+            if (sender.CurrentState == MediaPlayerState.Playing)
+            {
+                _systemMediaTransportControl.PlaybackStatus = MediaPlaybackStatus.Playing;
+            }
+            else if (sender.CurrentState == MediaPlayerState.Paused)
+            {
+                _systemMediaTransportControl.PlaybackStatus = MediaPlaybackStatus.Paused;
+            }
+        }
+
+        /// <summary>
+        ///     Handle the buttons on the Universal Volume Control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void MediaTransportControlButtonPressed(SystemMediaTransportControls sender,
+            SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+            switch (args.Button)
+            {
+                case SystemMediaTransportControlsButton.Play:
+                    BackgroundMediaPlayer.Current.Play();
+                    break;
+                case SystemMediaTransportControlsButton.Pause:
+                    BackgroundMediaPlayer.Current.Pause();
+                    break;
+            }
+        }
+
+
+        private void Taskcompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            BackgroundMediaPlayer.Shutdown();
+            _deferral.Complete();
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            // You get some time here to save your state before process and resources are reclaimed
+            BackgroundMediaPlayer.Shutdown();
+            _deferral.Complete();
+        }
+    }
+}
